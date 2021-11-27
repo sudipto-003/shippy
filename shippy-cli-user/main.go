cfe3bc5350c79d6b366810dc0de8cb152473acaf
Sudipto <sudipto00003@gmail.com>
2021-11-27 23:16:30 +0600

    user services test from cli

--- shippy-cli-user/main.go
+++ shippy-cli-user/main.go
@@ -5,26 +5,54 @@ import (
 	"fmt"
 	"log"
 
-	proto "github.com/EwanValentine/shippy/shippy-service-user/proto/user"
 	"github.com/micro/cli/v2"
 	"github.com/micro/go-micro/v2"
+	pb "github.com/sudipto-003/shippy/shippy-service-user/proto/user"
 )
 
-func createUser(ctx context.Context, service micro.Service, user *proto.User) error {
-	client := proto.NewUserService("shippy.service.user", service.Client())
+func createUser(ctx context.Context, service micro.Service, user *pb.User) error {
+	client := pb.NewUserService("shippy.service.user", service.Client())
+	users, err := client.GetAll(ctx, &pb.Request{})
+	if err != nil {
+		return err
+	}
+	for _, v := range users.Users {
+		log.Println(v)
+	}
+
 	rsp, err := client.Create(ctx, user)
+
 	if err != nil {
 		return err
 	}
 
-	// print the response
 	fmt.Println("Response: ", rsp.User)
+	return nil
+}
+
+func authUser(ctx context.Context, service micro.Service, user *pb.User) error {
+	client := pb.NewUserService("shippy.service.user", service.Client())
+	token, err := client.Auth(ctx, user)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Token: ", token.Token)
+	valid, err := client.ValidateToken(ctx, &pb.Token{
+		Token: token.Token,
+	})
+	if err != nil {
+		return err
+	}
+	if valid.Valide == true {
+		fmt.Println("Token Validated")
+	}
 
 	return nil
 }
 
 func main() {
-	// create and initialise a new service
 	service := micro.NewService(
 		micro.Flags(
 			&cli.StringFlag{
@@ -33,11 +61,11 @@ func main() {
 			},
 			&cli.StringFlag{
 				Name:  "email",
-				Usage: "E-Mail",
+				Usage: "Your Email",
 			},
 			&cli.StringFlag{
 				Name:  "company",
-				Usage: "Company Name",
+				Usage: "Your Company",
 			},
 			&cli.StringFlag{
 				Name:  "password",
@@ -45,19 +73,17 @@ func main() {
 			},
 		),
 	)
-
 	service.Init(
 		micro.Action(func(c *cli.Context) error {
-			log.Println(c)
 			name := c.String("name")
 			email := c.String("email")
 			company := c.String("company")
 			password := c.String("password")
 
-			log.Println("test:", name, email, company, password)
+			log.Println("Test: ", name, email, company, password)
 
 			ctx := context.Background()
-			user := &proto.User{
+			user := &pb.User{
 				Name:     name,
 				Email:    email,
 				Company:  company,
@@ -66,6 +92,14 @@ func main() {
 
 			if err := createUser(ctx, service, user); err != nil {
 				log.Println("error creating user: ", err.Error())
+				return err
+			}
+
+			if err := authUser(ctx, service, &pb.User{
+				Email:    email,
+				Password: password,
+			}); err != nil {
+				log.Println("error authenticate user: ", err.Error())
 				return err
 			}
 
